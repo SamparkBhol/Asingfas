fea97ad070b80ef4acbfb5b233117c35a80f8e72
Sampark Bhol <143442448+SamparkBhol@users.noreply.github.com>
2024-08-16 22:51:24 +0530

    Create task_provider.dart

---
+++ task_provider.dart
@@ -0,0 +1,23 @@
+// lib/providers/task_provider.dart
+
+import 'package:flutter/material.dart';
+import '../models/task.dart';
+
+class TaskProvider with ChangeNotifier {
+  List<Task> _tasks = [];
+
+  List<Task> get tasks => _tasks;
+
+  void addTask(Task task) {
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void toggleTaskStatus(String id) {
+    final index = _tasks.indexWhere((task) => task.id == id);
+    if (index != -1) {
+      _tasks[index].isCompleted = !_tasks[index].isCompleted;
+      notifyListeners();
+    }
+  }
+}
