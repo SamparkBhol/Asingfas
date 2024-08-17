519535d55c9fa008dba06775998c70443b7a24d5
Sampark Bhol <143442448+SamparkBhol@users.noreply.github.com>
2024-08-17 17:46:51 +0530

    Create TeamMember.dart

---
+++ 17aug/TeamMember.dart
@@ -0,0 +1,7 @@
+class TeamMember {
+  final String name;
+  final String linkedInUrl;
+  final String githubUrl;
+
+  TeamMember({required this.name, required this.linkedInUrl, required this.githubUrl});
+}
